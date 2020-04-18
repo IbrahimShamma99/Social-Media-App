7c25223dcf3f8db6dc0d0472106edd962fca237b
ibrahimshamma99 <i.abushammah@gmail.com>
2020-04-18 04:57:21 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const config = require('./../config/config');
+const app = require('./express');
+const mongoose = require('mongoose');
+
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri)
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`)
+})
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', config.port)
+})
