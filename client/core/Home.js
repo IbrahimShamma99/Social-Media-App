f9c87689caff9f7401ab7bd9665b59c230a95ff9
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-18 06:55:19 +0300

    update

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,79 +1,85 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {withStyles} from 'material-ui/styles';
-import Card, {CardContent, CardMedia} from 'material-ui/Card';
-import Typography from 'material-ui/Typography';
-import seashellImg from './../assets/images/sm.png';
-import Grid from 'material-ui/Grid';
-import auth from './../auth/auth-helper';
-import FindPeople from './../user/FindPeople';
-import Newsfeed from './../post/Newsfeed';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "material-ui/styles";
+import Card, { CardContent, CardMedia } from "material-ui/Card";
+import Typography from "material-ui/Typography";
+import seashellImg from "./../assets/images/sm.png";
+import Grid from "material-ui/Grid";
+import auth from "./../auth/auth-helper";
+import FindPeople from "./../user/FindPeople";
+import Newsfeed from "./../post/Newsfeed";
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     flexGrow: 1,
     margin: 30,
   },
   card: {
-    margin: 'auto',
-    marginTop: theme.spacing.unit * 5
+    margin: "auto",
+    marginTop: theme.spacing.unit * 5,
   },
   title: {
-    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
-    color: theme.palette.text.secondary
+    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${
+      theme.spacing.unit * 2
+    }px`,
+    color: theme.palette.text.secondary,
   },
   media: {
-    width:"100%"    
-  }
-})
+    width: "100%",
+  },
+});
 
 class Home extends Component {
   state = {
-    defaultPage: true
-  }
+    defaultPage: true,
+  };
   init = () => {
-    if(auth.isAuthenticated()){
-      this.setState({defaultPage: false})
-    }else{
-      this.setState({defaultPage: true})
+    if (auth.isAuthenticated()) {
+      this.setState({ defaultPage: false });
+    } else {
+      this.setState({ defaultPage: true });
     }
-  }
+  };
   componentWillReceiveProps = () => {
-    this.init()
-  }
+    this.init();
+  };
   componentDidMount = () => {
-    this.init()
-  }
+    this.init();
+  };
   render() {
-    const {classes} = this.props
+    const { classes } = this.props;
     return (
       <div className={classes.root}>
-        {this.state.defaultPage &&
+        {this.state.defaultPage && (
           <Grid container spacing={24}>
             <Grid item xs={12}>
               <Card className={classes.card}>
-                <img className={classes.media} src={seashellImg} title="Unicorn Shells"/>
+                <img
+                  className={classes.media}
+                  src={seashellImg}
+                  title="Unicorn Shells"
+                />
               </Card>
             </Grid>
           </Grid>
-        }
-        {!this.state.defaultPage &&
+        )}
+        {!this.state.defaultPage && (
           <Grid container spacing={24}>
             <Grid item xs={8} sm={7}>
-              <Newsfeed/>
+              <Newsfeed />
             </Grid>
             <Grid item xs={6} sm={5}>
-              <FindPeople/>
+              <FindPeople />
             </Grid>
           </Grid>
-        }
+        )}
       </div>
-    )
+    );
   }
 }
 
 Home.propTypes = {
-  classes: PropTypes.object.isRequired
-}
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Home);
